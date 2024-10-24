47f312709c7eec3dff2df874060eb6d43efacca7
mikelna <mikelnavarro2006@gmail.com>
2024-10-24 11:51:13 +0200

    Add files via upload

---
+++ src/ej/Ejercicio_Ejemplos.java
@@ -0,0 +1,18 @@
+package ej;
+
+public class Ejercicio_Ejemplos {
+
+	public static void main(String[] args) {
+
+		int x = siguiente(8);
+		System.out.println(x);
+		
+		
+	} //Cierre del main
+	
+	public static int siguiente(int num) {
+
+		return num + 1;
+	} //Cierre de función
+
+}
---
+++ src/ej/T4EJ1.java
@@ -0,0 +1,17 @@
+package ej;
+
+public class T4EJ1 {
+
+	public static void main(String[] args) {
+
+		int i = repetir(3);
+		System.out.println("Hola Mundo");
+		
+	}//Cierre de main
+
+	//Definir una funcion
+	public static int repetir(int x) {
+		return x;
+	}
+	
+}//Cierre de clase
